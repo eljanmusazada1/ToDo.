cde038795e8a80340d6bc1c83950ee1e809ab07d
eljanmusazada1 <138122027+eljanmusazada1@users.noreply.github.com>
2024-01-13 22:38:52 +0400

    Add files via upload

---
+++ ToDo-main/app.js
@@ -0,0 +1,77 @@
+
+"use strict";
+$(document).ready(function () {
+    
+    $('.btn').click(function (e) { 
+        e.preventDefault();
+        if (!$.trim($('input').val()) == '') {
+            let a = b => {
+                for (let i=0 ; i<b ; i++) {$('.box').append(`<h2>${b}</h2>`)}
+            }
+            let v = f => Number(f) ? a(f) : $('.box').append(`<h2>${f}</h2>`);
+            v($('input').val())
+        }
+        $('input').val('')
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function () {
+//     $('.btn').click(function (e) {
+//         e.preventDefault();
+//         if (!$.trim($('input').val()) == '') {
+//             let a = b => {
+//                 for (let i=0; i<b; i++) {$('.box').append(`<h2>${b}</h2>`)}
+//             }
+//             let v = f => Number(f) ? a(f) : $('.box').append(`<h2>${f}</h2>`);
+//             v($('#myInput').val();)
+//         }
+//         $('#myInput').val(' ')
+//     })
+// });
+
+
+
+
+
+// let a =$('input').val();
+//         $('#myInput').val('');
+//         if ( (a/2)> 0 ) {
+//             for (let i = 0; i < a; i++) {
+                
+//                 $(".box").append(`<h2>${a}</h2>`);
+//             }
+           
+//         } else {
+//             $(".box").append(`<h2>${a}</h2>`);
+//         }
+        
+
+
+
+
+
+
+
